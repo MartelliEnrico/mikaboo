f27b37d1ed9b1a7a4918df5c5e21816f34bdf618
Enrico Martelli <mrcnrt95e30@gmail.com>
2017-01-20 12:43:34 +0100

    Implemented msg_t utility methods

--- lib/phase1/messages.c
+++ lib/phase1/messages.c
@@ -1,13 +1,60 @@
 #include "mikabooq.h"
+#include "const.h"
+
+static struct list_head msg_free;
 
 void msgq_init(void) {
+	static struct msg_t msgs[MAXMSG];
 
+	INIT_LIST_HEAD(&msg_free);
+
+	for(int i = 0; i < MAXMSG; i++) {
+		list_add(&msgs[i].m_next, &msg_free);
+	}
 }
 
 int msgq_add(struct tcb_t *sender, struct tcb_t *destination, uintptr_t value) {
+	if(sender == NULL || destination == NULL || list_empty(&msg_free)) {
+		return -1;
+	}
 
+	struct msg_t *msg = container_of(list_next(&msg_free), struct msg_t, m_next);
+	list_del(&msg->m_next);
+	msg->m_sender = sender;
+	msg->m_value = value;
+	list_add_tail(&msg->m_next, &destination->t_msgq);
+
+	return 0;
 }
 
 int msgq_get(struct tcb_t **sender, struct tcb_t *destination, uintptr_t *value) {
+	if(destination == NULL || list_empty(&destination->t_msgq)) {
+		return -1;
+	}
 
+	struct msg_t *msg = NULL;
+
+	if(sender == NULL || *sender == NULL) {
+		msg = container_of(list_next(&destination->t_msgq), struct msg_t, m_next);
+		if(sender != NULL) {
+			*sender = msg->m_sender;
+		}
+	} else {
+		struct msg_t *item;
+		list_for_each_entry(item, &destination->t_msgq, m_next) {
+			if(item->m_sender == *sender) {
+				msg = item;
+			}
+		}
+	}
+
+	if(msg == NULL) {
+		return -1;
+	}
+
+	*value = msg->m_value;
+	list_del(&msg->m_next);
+	list_add_tail(&msg->m_next, &msg_free);
+
+	return 0;
 }
