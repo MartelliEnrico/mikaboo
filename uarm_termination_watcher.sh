9ee33e26007ba514eaf4420d904c9bdf06a1f6cf
Enrico Martelli <mrcnrt95e30@gmail.com>
2017-01-23 09:31:30 +0100

    Added colored output

--- uarm_termination_watcher.sh
+++ uarm_termination_watcher.sh
@@ -4,12 +4,22 @@ TERM=${1:-term0.uarm}
 > $TERM
 uarm -c machine.uarm.cfg -e &
 UARM_PID=$!
-trap "kill $UARM_PID" EXIT
+trap "kill $UARM_PID 2>&1 >/dev/null" EXIT
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+RESET="\033[0m"
 while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
-	echo $LINE
 	case $LINE in
-		"SYSTEM HALTED.") exit 0 ;;
-		"KERNEL PANIC!") exit 1 ;;
+		"SYSTEM HALTED.")
+			echo -e "${GREEN}${LINE}${RESET}"
+			exit 0 ;;
+		"KERNEL PANIC!")
+			echo -e "${RED}${LINE}${RESET}"
+			exit 1 ;;
+		"UNKNOWN SERVICE.")
+			echo -e "${RED}${LINE}" ;;
+		*)
+			echo $LINE ;;
 	esac
 done < <(tail -f $TERM)
